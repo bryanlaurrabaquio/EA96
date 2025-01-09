094251d4ff416092f05f3bde83d789fa0cd4ea86
usuario <321096799@comunidad.unam.mx>
2025-01-08 21:24:30 -0600

    Programa con el *2 que no jalo

--- Practica_1.cpp
+++ Practica_1.cpp
@@ -1,23 +1,15 @@
 #include <stdio.h>
 int main() {
-  int validatePositiveInt;
-  int a;
-  printf("Ingresa un numero entero: ");
-  scanf("%c", &a);
-  validatePositiveInt = a;
-
-  void generateEvenNumbers(int limit); {
+    int numeroDeTabla;
+    int validatePositiveInt;
+    int a;
+    printf("Ingresa un numero entero: ");
+    scanf("%c", &a);
+    void generateEvenNumbers (int limit);
+    for (int a = 1; a == 1; a++){}
+    int resultado = a * 2;
     printf("Los numeros pares son: %i\n");
-    int limit;
-    for (int i = 1; i <= limit; i++) {
-      if (i % 2 == 0) {
-      }
-    }
-  }
-  printf("%i\n", a);
-
-  printf("Desea repetir el programa para salir?\n");
-  scanf("%i",&a);
-  return 0;
+    printf("Desea repetir el programa para salir?\n");
+    scanf("%i",&a);
+    return 0;
 }
-
