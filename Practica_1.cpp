b5cdea5c412e60445f50753babe24fcf0de6b4eb
usuario <321096799@comunidad.unam.mx>
2025-01-08 21:17:27 -0600

    Generacion de Numeros Pares hasta un limite

---
+++ Practica_1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+  int validatePositiveInt;
+  int a;
+  printf("Ingresa un numero entero: ");
+  scanf("%c", &a);
+  validatePositiveInt = a;
+
+  void generateEvenNumbers(int limit); {
+    printf("Los numeros pares son: %i\n");
+    int limit;
+    for (int i = 1; i <= limit; i++) {
+      if (i % 2 == 0) {
+      }
+    }
+  }
+  printf("%i\n", a);
+
+  printf("Desea repetir el programa para salir?\n");
+  scanf("%i",&a);
+  return 0;
+}
+
